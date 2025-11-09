9b288e81808202fa0f0e87f19c864869e9936d11
Tiberiu Cudalbu <oana.cudalbu3@gmail.com>
2025-11-09 18:28:02 +0200

    no

--- game/main.cpp
+++ game/main.cpp
@@ -7,9 +7,9 @@ int main() {
     tiage::WinConsole renderer(50,50);
     sokoban::Game game(renderer);
     renderer.setCursorVisible(false);
-    game.loadLevel("C:/Users/oana/Desktop/debug.lvl");
-    game.runGame();
     game.loadLevel("C:/Users/oana/Desktop/1.lvl");
     game.runGame();
+    //game.loadLevel("C:/Users/oana/Desktop/1.lvl");
+    //game.runGame();
     return 0;
 }

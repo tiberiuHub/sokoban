0464fcabf07a5d0733e93dc62ef52d01895cb302
Tiberiu Cudalbu <oana.cudalbu3@gmail.com>
2025-11-10 22:06:57 +0200

    no

--- game/main.cpp
+++ game/main.cpp
@@ -4,11 +4,19 @@
 #include "Game.h"
 
 int main() {
-    tiage::WinConsole renderer(50,50);
+    tiage::WinConsole renderer(50,100);
     sokoban::Game game(renderer);
     renderer.setCursorVisible(false);
     game.loadLevel("C:/Users/oana/Desktop/1.lvl");
-    game.runGame();
+    //game.runGame();
+    renderer.create(50, 50);
+    renderer.putChar(0, 0, tiage::Color::Green, 'a');
+    renderer.flush();
+    //renderer.putChar(0, 0, tiage::Color::Green, 'a');
+    //renderer.putChar(1, 1, tiage::Color::Green, 'a');
+   // renderer.putChar(1, 0, tiage::Color::Green, 'a');
+    //renderer.flush();
+
     //game.loadLevel("C:/Users/oana/Desktop/1.lvl");
     //game.runGame();
     return 0;

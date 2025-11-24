785d901c686369bd9bcd148281d52c943dc28d15
Tiberiu Cudalbu <oana.cudalbu3@gmail.com>
2025-11-24 21:42:26 +0200

    added viewport .cpp

--- game/main.cpp
+++ game/main.cpp
@@ -3,11 +3,12 @@
 #include <tiage/WinConsole.h>
 #include "Game.h"
 
-int main() {
+int 
+main() {
     tiage::WinConsole renderer(5,5);
     sokoban::Game game(renderer);
     renderer.setCursorVisible(false);
-    game.loadLevel("C:/Users/root/Desktop/1.lvl");
+    game.loadLevel("C:/Users/Oana/Desktop/1.lvl");
     game.runGame();
     return 0;
 }

d1353dae56b81fd5c91a2bdee6b616d098367fa2
Tiberiu Cudalbu <oana.cudalbu3@gmail.com>
2025-11-26 22:16:40 +0200

    added windows file select, in testing

--- game/main.cpp
+++ game/main.cpp
@@ -1,14 +1,16 @@
 // Copyright Tiberiu 2025
 
 #include <tiage/WinConsole.h>
+#include <tiage/WinFileSystem.h>
 #include "Game.h"
 
 int 
 main() {
+    tiage::WinFileSystem fileSys;
+    std::string answer = fileSys.pickFile("lvl");
+    std::string answer2 = fileSys.pickFolder();
     tiage::WinConsole renderer(5,5);
     sokoban::Game game(renderer);
     renderer.setCursorVisible(false);
-    game.loadLevel("C:/Users/Oana/Desktop/1.lvl");
-    game.runGame();
     return 0;
 }
